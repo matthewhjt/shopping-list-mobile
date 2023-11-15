65141b69773f5f88598621d3c2a2b885a36c2281
Matthew Turnip <matthewhjt72@gmail.com>
2023-11-15 15:29:31 +0700

    tutorial 8

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list/screens/menu.dart';
 // TODO: Impor drawer yang sudah dibuat sebelumnya
 import 'package:shopping_list/widgets/left_drawer.dart';
 
@@ -16,6 +21,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
   String _description = "";
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -115,8 +121,31 @@ class _ShopFormPageState extends State<ShopFormPage> {
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.indigo),
                 ),
-                onPressed: () {
+                onPressed: () async {
                   if (_formKey.currentState!.validate()) {
+                    // Kirim ke Django dan tunggu respons
+                    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                    final response = await request.postJson(
+                        "http://matthew-hotmaraja-tutorial.pbp.cs.ui.ac.id/create-flutter/",
+                        jsonEncode(<String, String>{
+                          'name': _name,
+                          'price': _price.toString(),
+                          'description': _description,
+                          // TODO: Sesuaikan field data sesuai dengan aplikasimu
+                        }));
+                    if (response['status'] == 'success') {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Produk baru berhasil disimpan!"),
+                      ));
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Terdapat kesalahan, silakan coba lagi."),
+                      ));
+                    }
                     showDialog(
                       context: context,
                       builder: (context) {
